34426d10f2f582e7d1226eec277c3636220488fb
HarDa20 <hharold855@gmail.com>
2026-04-08 15:59:02 -0500

    creacion de modelo de sucursal

---
+++ franquicias-api/src/main/java/com/DavidHernandez/franquicias_api/model/Sucursal.java
@@ -0,0 +1,63 @@
+package com.DavidHernandez.franquicias_api.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Sucursal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "franquicia_id")
+    private Franquicia franquicia;
+
+    @OneToMany(mappedBy = "sucursal", cascade = CascadeType.ALL)
+    private List<Producto> productos;
+
+    public Sucursal() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Franquicia getFranquicia() {
+        return franquicia;
+    }
+
+    public void setFranquicia(Franquicia franquicia) {
+        this.franquicia = franquicia;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+}
